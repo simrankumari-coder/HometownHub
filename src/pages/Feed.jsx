6a10533a0c255b0841d73da65d732badb6e6840f
simran <simrankumari.it1011@gmail.com>
2026-07-24 21:30:39 +0530

    feat:login page created

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,23 +1,82 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FaUser } from "react-icons/fa"
 import { FaHeart } from "react-icons/fa";
+import { FaRegHeart } from "react-icons/fa";
+import { FaRegCommentDots } from "react-icons/fa";
 import feedData from '../data/feed'
 const Feed = () => {
-    const [toggle, setToggle] = useState(feedData)
+    const [toggle, setToggle] = useState(() => {
+        const saved = localStorage.getItem("data")
+        return saved ? JSON.parse(saved) : feedData
+    })
+    console.log("refresh", toggle[0].isToggle)
+    const [showComment, setShowComment] = useState(null)
+
+    const [input, setInput] = useState([])
+
+    useEffect(() => {
+        localStorage.setItem("data", JSON.stringify(toggle))
+
+
+    }, [toggle])
+
     const toggleLike = (id) => {
         const newObj = toggle.map(toggle => {
             if (toggle.id === id) {
-                return !toggle.isToggle
-            } else {
-                toggle
+                return { ...toggle, isToggle: !toggle.isToggle, likes: !toggle.isToggle === true ? toggle.likes + 1 : toggle.likes - 1 }
+            }
+
+            else {
+                return toggle
             }
         })
-        setToggle(...toggle, newObj)
+        setToggle(newObj)
     }
-    console.log(
-        !toggle.isToggle
-    )
+    const toggleComment = (id) => {
+        if (showComment === id) {
+            setShowComment(null)
+        } else {
+            setShowComment(id)
+        }
+    }
+    const handleInput = (e, id) => {
+        const value = e.target.value;
+        const existId = input.find(text => text.id === id)
+        if (existId) {
+            setInput(input.map(item => {
+                return item.id === id ?
+                    { ...item, text: value } : item
+
+            }))
+        } else {
+            setInput([...input, { id, text: value }])
+
+        }
+
+    }
+    const handleBtn = (id) => {
+        const currentText = input.find(item => item.id === id)
+        if (!currentText) {
+            return;
+        }
+
+        console.log("text ", currentText.text)
+        const newComment = toggle.map(toggle => {
+            if (toggle.id === id) {
+
+                return {
+                    ...toggle, comments: [
+                        ...toggle.comments, { author: "simran kumari", text: currentText.text }]
+                }
+            }
+            return toggle
+        })
+        setInput(input.map(text => text.id === id ? { ...text, text: "" } : text))
+
+        setToggle(newComment)
+
+    }
     return (
         <div className='bg-white px-4 py-7 flex flex-col gap-8'>
             <div className=''>
@@ -25,24 +84,48 @@ const Feed = () => {
                 <p className='text-sm text-gray-600'>"Stay connected with your hoemtown community."</p>
             </div>
             <section>
-                <div className='flex flex-col justify-center gap-5 items-center max-w-5xl w-full'>
+                <div className='flex flex-col justify-center gap-5 items-center p-3 md:p-6 max-w-3xl mx-auto w-full'>
                     {toggle.map((post) => {
-                        return <div key={post.id} className='px-5 py-5 bg-slate-50 max-w-5xl w-full shadow-lg rounded-md '>
+                        return <div key={post.id} className='px-5 py-5 bg-slate-50 max-w-5xl w-full shadow-md hover:shadow-xl transition duration-300 rounded-xl '>
                             <div className='flex justify-between'>
 
-                                <h2 className='flex gap-2 text-lg font-semibold'><FaUser className='mt-1 text-black' />{post.author}</h2>
+                                <h2 className='flex gap-2 text-[18px] font-bold'><FaUser className='mt-1 text-black' />{post.author}</h2>
 
-                                <p className=' text-sm text-gray-500'>{post.time}</p>
+                                <p className=' text-[13px] text-gray-500'>{post.time}</p>
                             </div>
-                            <p>{post.community}</p>
-                            <p>{post.content}</p>gg
-                            <img className='max-w-sm w-full rounded-lg' src={post.image} alt={post.community} />
-                            <div>
-                                <span><FaHeart onClick={() => toggleLike(post.id)} className={`${post.isToggle === true ? "text-red-600" : "text-white"} `} /></span>
+                            <p className='text-blue-700 text-[14px]'>{post.community}</p>
+                            <p className='mt-3 leading-7 text-gray-700 '>{post.content}</p>
+                            <img className=' w-full rounded-xl h-72 object-cover mt-4' src={post.image} alt={post.community} />
+                            <div className='flex gap-20 pt-4 mt-5 border-t border-slate-200 text-black'>
 
-                                <span>{post.likes}</span>
+                                <span className='flex gap-1 text-xl'>
+                                    {post.isToggle === true ?
+                                        <FaHeart onClick={() => toggleLike(post.id)} className='hover:scale-110 transition text-red-600 cursor-pointer mt-1' /> : <FaRegHeart onClick={() => toggleLike(post.id)} className='mt-1 cursor-pointer hover:scale-110 transition' />
+                                    }
+                                    {post.likes}
+                                </span>
+                                <section>
+                                    <span className='flex gap-1 text-xl'><FaRegCommentDots className='hover:scale-110 transition text-black mt-1' onClick={() => toggleComment(post.id)} />{post.comments.length}
+                                    </span>
 
-                                <span>comments</span>
+                                    {showComment === post.id &&
+                                        <div className='mt-4 border-t p-3 pt-4 bg-slate-100 rounded-xl border-slate-200  shadow-sm '>
+                                            {post.comments.map(comment =>
+                                                <div className='mb-3 border-b border-slate-200 pb-2' key={comment.id}>
+                                                    <h2 className='font-semibold text-sm text-slate-800'>{comment.author}</h2>
+                                                    <p className='text-gray-600 mt-1 text-sm'>{comment.text}</p>
+                                                </div>
+                                            )
+                                            }
+                                            <div className='flex gap-2 mt-5'>
+
+                                                <input className='flex-1 border border-slate-300 focus:ring-2 focus:ring-blue-500 outline-none rounded-lg px-3 py-2' value={input.find(item => item.id === post.id)?.text || ""} onChange={(e) => handleInput(e, post.id)} placeholder='add comments' />
+
+                                                <button className='bg-blue-700 hover:bg-blue-800 transition text-white px-4 rounded-lg' onClick={() => handleBtn(post.id)}>Add</button>
+                                            </div>
+                                        </div>
+                                    }
+                                </section>
                             </div>
                         </div>
                     })}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,65 @@
 import React from 'react'
-
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { FaHandsClapping } from "react-icons/fa6";
 const Login = () => {
-    return (
-        <div>
+    const [input, setInput] = useState(() => {
+        let data = localStorage.getItem("username")
+        return data ? JSON.parse(data) : ""
+    })
+    const [password, setPassword] = useState(() => {
+        let data = localStorage.getItem("password")
+        return data ? JSON.parse(data) : ""
+    })
 
+    useEffect(() => {
+        localStorage.setItem(JSON.stringify("username"))
+    }, [username])
+    useEffect(() => {
+        localStorage.setItem(JSON.stringify("password"))
+    }, [password])
+
+
+    const handleInput = (e) => {
+        setInput(e.target.value)
+    }
+    const handlePass = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const isDisabled = (input === '' || password === '');
+    return (
+        <div className='bg-slate-50'>
+            <div className=' flex flex-col justify-center items-center px-4'>
+
+                <h2 className='mt-4 justify-center items-center flex gap-2 text-2xl md:text-4xl text-blue-900 font-bold'>Welcome Back <FaHandsClapping className='mt-2' /></h2>
+                <p className='text-xl font-semibold  text-gray-600'>SignIn to HometownHub</p>
+            </div>
+            <form className=' mb-4 w-full bg-white mx-auto max-w-md  shadow-lg rounded-xl p-8 flex flex-col gap-5 font-semibold items-center justify-center  h-auto mt-4 '>
+                <label className='flex flex-col gap-2 font-medium text-slate-700'>Username:
+                    <input className='border border-slate-300 rounded-md px-3 py-2 outline-none focus:ring-2 focus:ring-blue-600' type="text" value={input} onChange={handleInput} placeholder='Enter your username' />
+                    {(input.length < 2 && input != "") && <span>Username must has atleast two characters</span>}
+                </label>
+                <label className='flex flex-col gap-2 font-medium text-slate-700'>Password:
+                    <input className='border border-slate-300 rounded-md px-3 py-2 outline-none focus:ring-2 focus:ring-blue-600' placeholder='Enter your Password' type='password' value={password} onChange={handlePass} />
+                    {(password.length > 6 && password != "") && <span>Password is only six digits</span>}
+                </label>
+                <div className='flex gap-3 mt-2'>
+                    <label className='flex gap-2  font-medium text-slate-700'>
+                        <input type="checkbox" className='mt-1' />
+                        Remember Me
+                    </label>
+                    <h3 className=' font-medium text-slate-700'>Forget Password?</h3>
+                </div>
+                <button disabled={isDisabled} className='mt-3 bg-blue-800 text-white px-4 py-2 rounded-md hover:bg-blue-900 transition'>Sign In</button>
+                {isDisabled}
+                <div className='border-t border-slate-200 w-full flex flex-col justify-center items-center'>
+                    <h2>Don't have an account?</h2>
+                    <Link to='/register'>
+                        <h2 className='font-medium text-slate-700'>Register</h2>
+                    </Link>
+                </div>
+            </form>
         </div>
     )
 }
